54e6b25decbc4c48250c6c3b6a21512473e9e6cc
ERIZON ADE PRATAMA <erizonsmart88@gmail.com>
2023-05-26 07:19:33 +0700

    Initial commit from Create Next App

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google'
+import Headers from './ui/header';
+import Hero from './ui/hero';
+import Skills from './ui/skills';
+import About from './ui/about';
+import Portfolio from './ui/portfolio';
+import Footer from './ui/footer';
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  return (
+    <main className={`flex  min-h-screen flex-col items-center justify-between ${inter.className}`}>
+
+     <Headers/>
+
+     <Hero/>
+
+     <About/>
+
+     <Skills/>
+
+     <Portfolio/>
+
+     <Footer/>
+    </main>
+  )
+}
